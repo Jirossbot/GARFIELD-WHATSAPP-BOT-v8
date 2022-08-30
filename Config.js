1dd8fe242b3f93fd0d8fbfc50819a7d963835b0a
Jirossbot <111544574+Jirossbot@users.noreply.github.com>
2022-08-30 11:35:05 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -16,9 +16,9 @@ global.APIKeys = {
 global.emoji01 = ['🐼'] // 👈 Enter your Emoji 
 global.emoji02 = ['🦋'] //👈 Enter your Emoji
 global.notice = ` *Hi 🥰 I m*
-*ڪيتارو صبڪ Ξ*
+*𓀛 𝐽𝐼𝑅𝑂𝑆𝑆 𝐵𝑂𝑇 𓆙 Ξ*
 *I am 16 years old developer...*
-*From Sri Morocco 🇱🇰*
+*From Sri Morocco ⚠️😈*
 *I am a Learning.*
 *Not more..*
 *Also I am Student*
@@ -27,13 +27,13 @@ global.owner =  ['+212710550922'] //👈  Enter Your number
 global.premium =  ['+212710550922'] //👈  Enter Your number
 global.ownernomer = '+212710550922' //👈  Enter Your number
 global.ownername = 'ZENOI' //👈 Enter Your name
-global.botname = ' 𝐉𝐈𝐑𝐎𝐒𝐒 ＢＯＴ' //👈 Enter Your Bot name
-global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𝐉𝐈𝐑𝐎𝐒𝐒 𝖡𝗈𝗍' //👈 Enter Your Name
+global.botname = ' 𓀛 𝐽𝐼𝑅𝑂𝑆𝑆 𝐵𝑂𝑇 𓆙' //👈 Enter Your Bot name
+global.footer = '© 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖡𝗒 𓀛 𝐽𝐼𝑅𝑂𝑆𝑆 𝐵𝑂𝑇 𓆙' //👈 Enter Your Name
 global.ig = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8' // 👈 You Can change this your choice
 global.region = 'India, South India, Kerala' // 👈 You Can change this your choice
 global.sc = 'https://github.com/Zenoixnoize/GARFIELD-WHATSAPP-BOT-v8'
 global.fbt = 'Follw Us ℹ️' // 👈 Enter (Subscribe/Follow/Like now)
-global.myweb = 'https://www.facebook.com/garfieldbots/'// 👈 Enter your Social media link to follow now button
+global.myweb = 'Wa.me/+212710550922?text=.men'// 👈 Enter your Social media link to follow now button
 global.welcome = '*Welcome To The Group 🦋*' // 👈 You Can change this your choice
 global.bye = '*Bye bye Mother fucker 🐼*' // 👈 You Can change this your choice
 global.packname = 'GARFIELD-BOTv8'  // 👈 You Can change this your choice 
